7a7785f494e0301622d701deb945585fbdc27e02
Alex Bardi <alex.bardi@hey.com>
2022-07-19 22:06:03 -0500

    Can show an heic pic. Yahoo

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import pyheif
+from PIL import Image
+
+def main():
+    # pyheif.open_container("./../img/IMG_7157.HEIC")
+    heif_file = pyheif.read("./../img/IMG_7157.HEIC")
+    image = Image.frombytes(
+    heif_file.mode, 
+    heif_file.size, 
+    heif_file.data,
+    "raw",
+    heif_file.mode,
+    heif_file.stride,
+    )
+    image.show()
+
+if __name__=="__main__":
+    main()

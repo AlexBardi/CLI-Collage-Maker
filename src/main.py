9c0db4e8b99798928368bf5e277b1e00aefafeb9
Alex Bardi <alex.bardi@hey.com>
2022-07-20 20:57:35 -0500

    Hopefully that was all the collaging I needed to do

--- src/main.py
+++ src/main.py
@@ -2,18 +2,38 @@ import pyheif
 from PIL import Image
 
 def main():
-    image_single = Image.open("./../img/car.png") 
+    images = []
+
+    images.append(Image.open("./../img/IMG_7156.PNG"))
+
+    names = ["IMG_4164", "IMG_7157 2"]
+
+    for name in names:
+        fullpath = "./../img/" + name + ".heic"
+
+        heif_file = pyheif.read(fullpath)
+        image = Image.frombytes(
+        heif_file.mode, 
+        heif_file.size, 
+        heif_file.data,
+        "raw",
+        heif_file.mode,
+        heif_file.stride,
+        )
+
+        images.append(image)
 
     num_horiz = 5
     num_vert = 10
 
-    image_collage = Image.new("RGB", (image_single.size[0] * num_horiz, image_single.size[1] * num_vert), "white")
+    for image_single in images:
+        image_collage = Image.new("RGB", (image_single.size[0] * num_horiz, image_single.size[1] * num_vert), "white")
 
-    for i in range(num_horiz):
-        for j in range(num_vert): 
-            image_collage.paste(image_single, (i * image_single.size[0], j * image_single.size[1]))
+        for i in range(num_horiz):
+            for j in range(num_vert): 
+                image_collage.paste(image_single, (i * image_single.size[0], j * image_single.size[1]))
 
-    image_collage.show()
+        image_collage.show()
 
 
 if __name__=="__main__":

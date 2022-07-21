69e7886fdc4d43cacaaf783091b66fddfd2ba4bc
Alex Bardi <alex.bardi@hey.com>
2022-07-20 20:36:29 -0500

    random side utility

--- src/main.py
+++ src/main.py
@@ -2,16 +2,19 @@ import pyheif
 from PIL import Image
 
 def main():
-    # pyheif.open_container("./../img/IMG_7157.HEIC")
-    heif_file = pyheif.read("./../img/IMG_2328.heic")
-    image = Image.frombytes(
-    heif_file.mode, 
-    heif_file.size, 
-    heif_file.data,
-    "raw",
-    heif_file.mode,
-    heif_file.stride,
-    )
+    image_single = Image.open("./../img/car.png") 
+
+    num_horiz = 5
+    num_vert = 10
+
+    image_collage = Image.new("RGB", (image_single.size[0] * num_horiz, image_single.size[1] * num_vert), "white")
+
+    for i in range(num_horiz):
+        for j in range(num_vert): 
+            image_collage.paste(image_single, (i * image_single.size[0], j * image_single.size[1]))
+
+    image_collage.show()
+
 
 if __name__=="__main__":
     main()

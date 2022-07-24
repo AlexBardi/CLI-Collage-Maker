681eed81925b0a5725548ca6505e4b5f9a3b2f83
Alex Bardi <alex.bardi@hey.com>
2022-07-24 10:13:01 -0500

    Much more rubust and user friendly

--- src/main.py
+++ src/main.py
@@ -1,39 +1,47 @@
 import pyheif
+import os, os.path
 from PIL import Image
 
+def import_pics(path):
+    imgs = []
+    names = []
+    valid_images = [".jpg",".gif",".png",".tga"]
+    for f in os.listdir(path):
+        name = os.path.splitext(f)[0]
+        ext = os.path.splitext(f)[1]
+        if ext.lower() == ".heic":
+            heif_file = pyheif.read(path + f)
+            image = Image.frombytes(
+                heif_file.mode, 
+                heif_file.size, 
+                heif_file.data,
+                "raw",
+                heif_file.mode,
+                heif_file.stride,
+            )
+            imgs.append(image)
+        elif ext.lower() not in valid_images:
+            continue
+        else:
+            imgs.append(Image.open(os.path.join(path,f)))
+
+        names.append(name) 
+    return imgs, names
+
 def main():
-    images = []
+    images, names = import_pics("./../inputs/")
 
-    images.append(Image.open("./../img/IMG_7156.PNG"))
+    num_horiz = int(input("How many images across? "))
+    num_vert = int(input("How many images tall? "))
 
-    names = ["IMG_4164", "IMG_7157 2"]
-
-    for name in names:
-        fullpath = "./../img/" + name + ".heic"
-
-        heif_file = pyheif.read(fullpath)
-        image = Image.frombytes(
-        heif_file.mode, 
-        heif_file.size, 
-        heif_file.data,
-        "raw",
-        heif_file.mode,
-        heif_file.stride,
-        )
-
-        images.append(image)
-
-    num_horiz = 5
-    num_vert = 10
-
-    for image_single in images:
+    for image_single, name in zip(images, names):
         image_collage = Image.new("RGB", (image_single.size[0] * num_horiz, image_single.size[1] * num_vert), "white")
 
         for i in range(num_horiz):
             for j in range(num_vert): 
                 image_collage.paste(image_single, (i * image_single.size[0], j * image_single.size[1]))
 
-        image_collage.show()
+        image_collage.save("./../outputs/" + name + "-" + str(num_horiz) + "x" + str(num_vert) + ".png")
 
 
 if __name__=="__main__":
